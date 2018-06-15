f10a08cfe0a3ab700e0ad3c108283e9ab1dae76d
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-06-16 08:38:08 +0900

    Put lambda into vpc

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -15,6 +15,10 @@ resource "aws_lambda_function" "stream" {
     role             = "${aws_iam_role.lambda.arn}"
     description      = "DynamoDB To ElasticSearch"
 
+    vpc_config {
+        subnet_ids = ["${aws_subnet.pub3.id}", "${aws_subnet.pub4.id}"]
+        security_group_ids = ["${aws_security_group.allow_all.id}"]
+    }
     environment {
         variables = {
             ES_HOST = "${aws_elasticsearch_domain.es.endpoint}"
@@ -30,11 +34,11 @@ resource "aws_iam_role" "lambda" {
     "Version": "2012-10-17",
     "Statement": [
         {
+            "Effect": "Allow",
             "Action": "sts:AssumeRole",
             "Principal": {
                 "Service": "lambda.amazonaws.com"
             },
-            "Effect": "Allow",
             "Sid": ""
         }
     ]
@@ -42,6 +46,7 @@ resource "aws_iam_role" "lambda" {
 EOF
 }
 
+# Attach to role
 resource "aws_iam_role_policy" "lambda-log-output" {
     role = "${aws_iam_role.lambda.id}"
     name = "lambda-output"
@@ -50,16 +55,43 @@ resource "aws_iam_role_policy" "lambda-log-output" {
     "Version": "2012-10-17",
     "Statement": [
         {
+            "Effect": "Allow",
             "Action": [
                 "logs:CreateLogGroup",
                 "logs:CreateLogStream",
                 "logs:PutLogEvents",
                 "es:*"
             ],
-            "Resource": "arn:aws:logs:*:*:*",
-            "Effect": "Allow"
+            "Resource": "arn:aws:logs:*:*:*"
         }
     ]
 }
 EOF
 }
+resource "aws_iam_role_policy" "dynamodb-stream" {
+    role = "${aws_iam_role.lambda.id}"
+    name = "dynamodb-stream"
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:DescribeStream",
+                "dynamodb:GetRecords",
+                "dynamodb:GetShardIterator",
+                "dynamodb:ListStreams"
+            ],
+            "Resource": "${aws_dynamodb_table.table.stream_arn}"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_permissions" {
+    role       = "${aws_iam_role.lambda.name}"
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
